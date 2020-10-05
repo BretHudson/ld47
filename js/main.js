58b456d2d87f21c68cdaeddc9e0677bf7c468194
Bret Hudson <bret@brethudson.com>
2020-10-05 13:40:04 -0700

    "Game" is there

--- js/main.js
+++ js/main.js
@@ -5,19 +5,101 @@ Document.prototype.on = Document.prototype.addEventListener;
 DocumentFragment.prototype.on = DocumentFragment.prototype.addEventListener;
 
 /// Globals
-let score = 0, level = 1;
-let scoreElem, levelElem;
+let score = 0;
+let stationCount = 1, stationIndex = 0;
+let scoreElem;
 let gameElem, buttonElem;
+let cursorElem;
+let canMove = true;
+
+const stations = [];
 
 /// Functions
+const remToPx = rem => {
+	const { fontSize } = getComputedStyle(document.documentElement);
+	return rem * parseFloat(fontSize);
+};
+
+const resize = () => {
+	document.documentElement.style.fontSize = '16px';
+	
+	const { height: headerHeight } = getComputedStyle(document.querySelector('header'));
+	const { height: footerHeight } = getComputedStyle(document.querySelector('footer'));
+	
+	const width = window.innerWidth;
+	const height = window.innerHeight - (parseFloat(headerHeight) + parseFloat(footerHeight));
+	const ratio = width / height;
+	let size = width;
+	if (ratio > 1) size = height;
+	size /= 35;
+	
+	console.log(
+		window.innerWidth,
+		window.innerHeight
+	);
+	console.log({ size });
+	
+	document.documentElement.style.fontSize = `${size}px`;
+};
+
 const updateProgressText = () => {
 	scoreElem.textContent = score;
-	levelElem.textContent = level;
+};
+
+const addStation = () => {
+	const elem = document.createElement('div');
+	elem.className = 'station';
+	gameElem.append(elem);
+	stations.push(elem);
+	stations.unshift(stations.pop());
+	
+	const num = stations.length;
+	stations.forEach((station, i) => {
+		station.style.setProperty('--rotation', `${(i * 360.0) / num}deg`);
+	});
 };
 
 const spin = () => {
-	++score;
+	if (canMove === false) return;
+	
+	if (++stationIndex === stationCount) {
+		stationIndex = 0;
+		++score;
+		++stationCount;
+		canMove = false;
+		setTimeout(() => {
+			addStation();
+			canMove = true;
+		}, 500);
+	}
+	
+	const deg = (score + (stationIndex / stationCount)) * 360;
+	
+	cursorElem.style.setProperty('--rotation', `${deg}deg`);
+	
 	updateProgressText();
+};
+
+let isLoaded = -1;
+const onLoad = () => {
+	if (++isLoaded < 1) return;
+	
+	scoreElem = document.querySelector('#score');
+	
+	gameElem = document.querySelector('#game');
+	buttonElem = document.querySelector('#button');
+	cursorElem = document.querySelector('#cursor');
+	
+	updateProgressText();
+	
+	buttonElem.on('click', spin);
+	resize();
+	
+	window.requestAnimationFrame(() => {
+		document.body.classList.remove('loading');
+		
+		addStation();
+	});
 };
 
 /// Listeners
@@ -29,13 +111,7 @@ document.on('keydown', e => {
 	}
 });
 
-document.on('DOMContentLoaded', e => {
-	gameElem = document.querySelector('#game');
-	buttonElem = document.querySelector('#button');
-	scoreElem = document.querySelector('#score');
-	levelElem = document.querySelector('#level');
-	
-	updateProgressText();
-	
-	buttonElem.on('click', spin);
-});
+onLoad();
+document.on('DOMContentLoaded', onLoad);
+
+window.on('resize', resize);
